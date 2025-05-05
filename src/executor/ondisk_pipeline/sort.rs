0d38ced1560acf34d627b2c58f624ca46740d1a5
Charles Benello <cmbenello@uchicago.edu>
2025-05-05 01:33:33 -0500

    i think there is an issue with drop container rn

--- src/executor/ondisk_pipeline/sort.rs
+++ src/executor/ondisk_pipeline/sort.rs
@@ -645,7 +645,10 @@ impl<T: TxnStorageTrait, M: MemPool> OnDiskSort<T, M> {
         println!("Total tuples estimated: {}", total_tuples);
     
         // Decide on the number of threads
-        let num_threads = 40;
+        let num_threads = env::var("NUM_THREADS")
+            .unwrap_or_else(|_| "8".to_string())
+            .parse()
+            .expect("NUM_THREADS must be a valid number");
     
         // Calculate chunk size
         let chunk_size = (total_tuples + num_threads - 1) / num_threads;
@@ -1508,7 +1511,7 @@ impl<T: TxnStorageTrait, M: MemPool> OnDiskSort<T, M> {
             }
         };
         let duration_merge = start_merge.elapsed();
-        // println!("bp stats after merge {}", mem_pool.stats());
+        println!("bp stats after merge {}", mem_pool.stats());
 
         // // Print post-merge statistics
         // let post_merge_stats = StorageStats {
